2028ac88b29779b662bdbd1bece9564a1cd05d63
BrentVanwildemeersch <brent.vanwildemeersch@ocular.be>
2020-05-06 21:10:18 +0200

    Update

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,14 @@
 import vue from 'rollup-plugin-vue'
-import buble from 'rollup-plugin-buble'
-import commonjs from 'rollup-plugin-commonjs'
 
 export default {
-    input:"./src/vue-global-map.vue",
-    plugins:{
-        preVue:[
-            replace({
-                'process.env.NODE_ENV':JSON.stringify('production')
-            }),
-            commonjs() //support for commonJS modules
-        ],
-        vue:{
-            css:true,
-            template:{
-                isProduction:true
-            }
-        },
-        postVue:[
-            buble() //use buble to transpile ES2015
-        ]
+  input: 'src/vue-global-map.vue',
+  output: [
+    {
+      format: 'esm',
+      file: 'dist/vue-global-map.esm.js',
     },
+  ],
+  plugins: [
+    vue()
+  ]
 }
--- src/vue-global-map.vue
+++ src/vue-global-map.vue
@@ -1,12 +1,16 @@
 <template>
     <div>
-
+        <MapSVGComponent/>
     </div>
 </template>
 
 <script>
+import MapSVGComponent from './mapSVGComponent.vue'
     export default {
         name:'vue-global-map',
+        components: {
+            MapSVGComponent,
+        },
     }
 </script>
 
